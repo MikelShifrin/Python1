b5ef571f91b64ea9015a3a38622c69011bc21360
MikelShifrin <miki.super@gmail.com>
2019-07-07 17:57:44 -0400

    restructured exercices and created virtual environment

---
+++ Exercises/Exercise 10.py
@@ -0,0 +1,6 @@
+#Assignment 10
+#Calorie calculator
+#calories from carbs = carb grams x 4
+#calories from fat = fat grams x 9
+#calories from protein = protein x 4
+#total calories
---
+++ Exercises/Exercise 11.py
@@ -0,0 +1,10 @@
+#Assignment 11
+#Test Average and Letter Grade
+#ask for 5 grades
+#calculate and display the average
+#display letter grade
+#90 -100  A
+#80 - 89  B
+#70 - 79  C
+#60 - 69  D
+#Below 60 F
