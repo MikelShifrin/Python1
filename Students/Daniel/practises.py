5b6840dab15d905eff20063b846c0df9a8a40471
DanielJiang2008 <danieljiangmaster@gmail.com>
2019-07-08 06:35:13 -0700

    moved Daniel to students

--- Students/Daniel/practises.py
+++ Students/Daniel/practises.py
@@ -1,9 +1,7 @@
 #Nested loops
 
 number = int(input("Please enter a number:\n"))
-for i in range(0,number+1,1):
-    for x in range(0,i,1):
+for i in range(0,number+1):
+    for x in range(0,i):
         print("*",end="")
     print()
-
-#*#*if i put 2, it does'nt work
