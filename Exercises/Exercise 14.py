cd3dc048db9d949d7c33eaf8011b58f2d8f95cfb
MikelShifrin <miki.super@gmail.com>
2019-07-22 11:55:33 -0400

    added exercise 14 & 15

---
+++ Exercises/Exercise 14.py
@@ -0,0 +1,9 @@
+#Assignment 14
+#
+#Total Sales
+#
+#Design a program that asks the user to enter a store's sales for each
+#day of the week and display the result.
+
+
+
---
+++ Exercises/Exercise 15.py
@@ -0,0 +1,10 @@
+#Assignment 15
+#
+#Rainfall Statistics
+#
+#Design a program that lets the user enter the total rainfall for each
+#of the 12 months into a list. The program should calculate and display
+#the total rainfall for the year, the average monthly rainfall,
+#the months with the highest and lowest amounts.
+#In addittion display the each month's rainfall on a matplotlib
+#The rainfall on the y-axis & the month number on the x-axis
