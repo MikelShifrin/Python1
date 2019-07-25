babdfd78b8f7875f144b07235c2c78c7df4b4326
MikelShifrin <miki.super@gmail.com>
2019-07-25 10:29:01 -0400

    added string manipulation exercise

---
+++ Exercises/Exercise 16.py
@@ -0,0 +1,7 @@
+#Assignment 16
+#
+#Write a program that gets a string containing a person's
+#first, middle, and last names, and displays their first,
+#middle, and last initials
+#Ex: Mikel Dmitry Shifrin
+# M. D. S.
