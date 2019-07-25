661d6dc8b7ea34cf02883ffe500ecdd5ea6e18cb
MikelShifrin <miki.super@gmail.com>
2019-07-25 11:14:15 -0400

    added image reference in exercise 1

--- Exercises/Exercise 1 - .py
+++ Exercises/Exercise 1 - .py
@@ -1,4 +1,5 @@
 #Assignment 1
 #
 #Draw the olympic logo with color using Turtle
-#for a reference of the olympic logo use this link
+#for a reference of the olympic logo use the following link:
+#https://github.com/MikelShifrin/Python1/blob/master/Images/Olympic%20Logo.png
