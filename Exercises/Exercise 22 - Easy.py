439e59a4f93a0386c9c4cb673434feb960e30ae7
MikelShifrin <miki.super@gmail.com>
2019-08-06 11:14:36 -0400

    new dictionary exercise

---
+++ Exercises/Exercise 22 - Easy.py
@@ -0,0 +1,43 @@
+#Course Information
+
+#Write a program that creates a dictionary containing
+#course and the room numbers of those courses.
+#The dictionary should have the following key value pair:
+'''
+French        201
+English       202
+Chess         204
+Programming   209
+Robotics      209
+Art           205
+Badminton     Gym
+'''
+
+#The program should also create a dictionary containing
+#the course number and the names of the instructors
+#of each course. The dictionary should have the following
+#key value pairs:
+'''
+French        Jennifer
+English       Nikola
+Chess         Philip
+Programming   Mikel
+Robotics      Mikel
+Art           Li
+Badminton     Eddie
+'''
+
+#The program should also create a dictionary containing
+#course number and the time of the course. The dictionary
+#should have athe following key value pairs:
+'''
+French       9:00 am
+English      9:00 am
+Chess        3:00 pm
+Programming  9:00 am
+Robotics     1:00 pm
+Art          1:00 pm
+Badminton    2:00 pm
+'''
+#The program should let the user enter a course name, then
+#it should display the course's room, instructor, and start time
