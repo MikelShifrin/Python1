8cf995116cc43ae281cd19e3e1bb3c34d9104026
Zephyr-Begin-Zhang <zephyrbegin@gmail.com>
2019-08-06 10:51:53 -0400

    finished exercise 8

--- Students/Zephyr/Exercise 8.py
+++ Students/Zephyr/Exercise 8.py
@@ -1,8 +1,4 @@
-'''
 for i in range(7,0,-1):
-    i = '*'
-    print(i)
-'''
-for triangles in range(7+1,0,-1):
-    print('*',end='')
-         
+    for j in range(i):
+        print('*', end='')
+    print()
