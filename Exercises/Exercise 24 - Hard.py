b00f191c05cab3c42bbe87ef4bbfe3be25590934
MikelShifrin <miki.super@gmail.com>
2019-08-07 11:45:02 -0400

    new encryption exercise

---
+++ Exercises/Exercise 24 - Hard.py
@@ -0,0 +1,15 @@
+#File Encryption
+
+#Write a program that uses a dictionary to assign
+#"codes" to each letter of the alphabet. For example
+'''
+codes = {'A': '%', 'a': '^', 'B':'!', etc...}
+'''
+#In this example the letter A would become %
+#
+#The program should open a text file, read it,
+#Then use the dictionary to write an encrypted
+#version of the file's content to a second file.
+#Each character in the second file should contain
+#the code for the corresponding character in the
+#first file 
