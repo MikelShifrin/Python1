a38799d049827ed740127b7ac0ea7b972b237b3a
Eddy-Zhao <eddy.zhao2007@gmail.com>
2019-08-14 11:15:39 -0400

    added, updated and changed exercises and drew stuff

--- Students/Eddy/Exercises/Drawing 4.py
+++ Students/Eddy/Exercises/Drawing 4.py
@@ -1,16 +1,27 @@
 #Assignment 2
 #
 #Using Turtle draw the 4 patterns bellow:
-#
-#Pattern 1.jpeg
-#Pattern 2.jpeg
 #Pattern 3.jpeg
-#Pattern 4.jpeg
-#
-#The 4 images can be found at the following links:
 '''
-https://github.com/MikelShifrin/Python1/blob/master/Images/Pattern%201.jpg
-https://github.com/MikelShifrin/Python1/blob/master/Images/Pattern%202.jpg
 https://github.com/MikelShifrin/Python1/blob/master/Images/Pattern%203.jpg
-https://github.com/MikelShifrin/Python1/blob/master/Images/Pattern%204.jpg
 '''
+import turtle
+
+turtle.setup(10000,10000)
+turtle.speed(100)
+turtle.forward(265)
+turtle.right(90)
+turtle.forward(265)
+turtle.right(90)
+turtle.forward(265)
+turtle.right(90)
+turtle.forward(265)
+turtle.right(90)
+turtle.penup()
+turtle.right(90)
+turtle.forward(250)
+turtle.right(180)
+turtle.pendown()
+turtle.forward(250)
+turtle.right(90)
+turtle.forward(250)
