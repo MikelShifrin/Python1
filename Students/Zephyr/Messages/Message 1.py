27be16e3a5cba904ba18cf304a3078f02c613e2f
Zephyr-Begin-Zhang <zephyrbegin@gmail.com>
2019-08-15 17:24:19 -0400

    MIKEL PLEASE READ THIS

---
+++ Students/Zephyr/Messages/Message 1.py
@@ -0,0 +1,9 @@
+'''
+Hello Mikel,
+could you please explain the files things to me?
+I dont realy understand it and it's not clear for me.
+please send me a other message to answer me.
+thanks a lot
+'''
+
+(Sorry for the errors in my text...)
