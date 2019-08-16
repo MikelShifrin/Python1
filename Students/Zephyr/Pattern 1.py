0096a327023a28e0c639042ae01268b07e61943e
Zephyr-Begin-Zhang <zephyrbegin@gmail.com>
2019-08-16 11:17:59 -0400

    Random turtle drawings

---
+++ Students/Zephyr/Pattern 1.py
@@ -0,0 +1,15 @@
+import turtle as t
+
+t.setup(700,700)
+t.hideturtle()
+t.speed(20)
+
+while True:
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(5)
---
+++ Students/Zephyr/Pattern 2.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+t.setup(700,700)
+t.hideturtle()
+t.speed(20)
+
+while True:
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.pencolor('white')
+    t.forward(200)
+    t.left(90)
+    t.pencolor('black')
+    t.forward(200)
+    t.left(5)
